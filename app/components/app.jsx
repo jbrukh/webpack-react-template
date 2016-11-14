b9508e0cd1ec394deff380e686232e3eadd25b29
Jake Brukhman <jbrukh@gmail.com>
2016-11-14 17:38:21 -0500

    Working router.

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 import {Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
 import AboutPage from '../pages/AboutPage'
 import HomePage from '../pages/HomePage'
+import NotFound from '../pages/NotFound'
+import NavContainer from '../containers/NavContainer'
 
 require('../styles/global.scss')
 
 class App extends React.Component {
   render() {
     return (
-      <Router history={hashHistory}>
-        <Route path='/' component={HomePage} />
-        <Route path='/about' component={AboutPage} />
+      <Router history={browserHistory}>
+        <Route path='/' component={NavContainer}>
+          <IndexRoute component={HomePage} />
+          <Route path='/about' component={AboutPage} />
+          <Route path='*' component={NotFound} />
+        </Route>
       </Router>
     );
   }
